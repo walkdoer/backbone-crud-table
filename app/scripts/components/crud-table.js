7adbd2ac043c451278f43d9696b26df022cddc12
zhangmh <zhangmhao@gmail.com>
2014-05-08 12:40:25 +0800

    add file

---
+++ app/scripts/components/crud-table.js
@@ -0,0 +1,40 @@
+/**
+ * CRUD Table
+ *
+ * 提供CRUD功能的Table，基于Backbone
+ *
+ * var crudTable = new CRUDTable({
+ *     name: '联系人表',
+ *     columns: [{
+ *         name: 'address',
+ *         displayName: '地址'
+ *     }, {
+ *         name: 'tel',
+ *         displayName: '电话'
+ *     }, {
+ *         name: 'name',
+ *         displayName: '姓名'
+ *     }],
+ *     data: [{
+ *        name: 'andrew',
+ *        tel: '888',
+ *        address: '天堂路1号'
+ *     }, {
+ *        name: 'andrew',
+ *        tel: '888',
+ *        address: '天堂路1号'
+ *     }, {
+ *        name: 'andrew',
+ *        tel: '888',
+ *        address: '天堂路1号'
+ *     }]
+ * })
+ * @create 2014-05-08
+ */
+
+(function (window, undefined) {
+    'use strict';
+    window.CRUDTable = Backbone.View.extend({
+    
+    });
+})(window);
